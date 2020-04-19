d58b7ef3aeb97e142cf5e2e9c3e81a8d0759ee72
Matthew Lee <matthewlee626@gmail.com>
2020-04-18 21:21:48 -0700

    framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <div className="images">
+          <img src="./square.png" className="identify" alt-text="picture 1"/>
+          <img src="./square.png" className="identify" alt-text="test"/>
+          <img src="./square.png" className="identify" alt-text="test"/>
+        </div>
+        <div className="info">
+          <p>Which pictures are the same?</p>
+        </div>
+        <div className="options">
+          <select>
+            <option value="0">None</option>
+            <option value="1">1,2</option>
+            <option value="2">1,3</option>
+            <option value="3">2,3</option>
+            <option value="4">1,2,3</option>
+          </select>
+          <button type="submit">Submit</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Picture.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Picture extends React.Component {
+    render() {
+        return(
+            <img />
+        )
+    }
+}
+
+export default Picture;
