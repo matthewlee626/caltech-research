b43a10de8cee5e5d46adcfb5b5b01f7ee065a8fc
Matthew Lee <matthewlee626@gmail.com>
2020-04-19 22:13:16 -0700

    strawberry POSTing

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="images">
-          <img src="./square.png" className="identify" alt-text="picture 1"/>
-          <img src="./square.png" className="identify" alt-text="test"/>
-          <img src="./square.png" className="identify" alt-text="test"/>
+          <img src="./imgs/001.jpg" className="identify" alt-text="picture 1"/>
+          <img src="./imgs/001.jpg" className="identify" alt-text="test"/>
+          <img src="./imgs/001.jpg" className="identify" alt-text="test"/>
         </div>
         <div className="info">
           <p>Which pictures are the same?</p>
         </div>
         <div className="options">
-          <select>
-            <option value="0">None</option>
-            <option value="1">1,2</option>
-            <option value="2">1,3</option>
-            <option value="3">2,3</option>
-            <option value="4">1,2,3</option>
-          </select>
-          <button type="submit">Submit</button>
+        <form action="http://localhost:8080/api" method="POST">
+            <input type="text" id="name" name="name" placeholder="Name"/>
+            <select name="match" id="match">
+              <option value="0">None</option>
+              <option value="1">1,2</option>
+              <option value="2">1,3</option>
+              <option value="3">2,3</option>
+              <option value="4">1,2,3</option>
+            </select>
+            <button class="submit" type="submit">Submit</button>
+          </form>
         </div>
       </div>
     );
