9dd9c79f371499ddee42483b4da71d426019cde7
Matthew Lee <matthewlee626@gmail.com>
2020-04-19 23:02:13 -0700

    v1.0 strawberry picker

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import './App.css';
 import { render } from '@testing-library/react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: "",
+      second: "",
+      third: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      first: ("00" + parseInt(Math.random()*30)).slice(-3),
+      second: ("00" + parseInt(Math.random()*30)).slice(-3),
+      third: ("00" + parseInt(Math.random()*30)).slice(-3)
+    })
+  }
+
+
   render(){
     return (
       <div className="App">
         <div className="images">
-          <img src="./imgs/001.jpg" className="identify" alt-text="picture 1"/>
-          <img src="./imgs/001.jpg" className="identify" alt-text="test"/>
-          <img src="./imgs/001.jpg" className="identify" alt-text="test"/>
+          <img src={"./imgs/" + this.state.first +".jpg"} className="identify" alt-text="picture 1"/>
+          <img src={"./imgs/" + this.state.second +".jpg"} className="identify" alt-text="test"/>
+          <img src={"./imgs/" + this.state.third +".jpg"} className="identify" alt-text="test"/>
         </div>
         <div className="info">
           <p>Which pictures are the same?</p>
         </div>
         <div className="options">
-        <form action="http://localhost:8080/api" method="POST">
+        <form action="https://airtable-api-626.herokuapp.com/api" method="POST">
             <input type="text" id="name" name="name" placeholder="Name"/>
+            <input type="hidden" id="pic1" name="pic1" value={parseInt(this.state.first)}/>
+            <input type="hidden" id="pic2" name="pic2" value={parseInt(this.state.second)}/>
+            <input type="hidden" id="pic3" name="pic3" value={parseInt(this.state.third)}/>
             <select name="match" id="match">
               <option value="0">None</option>
               <option value="1">1,2</option>
