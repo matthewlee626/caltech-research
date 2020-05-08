1b0641a0329bac0589a0281f850a4ac8b3c7887b
Matthew Lee <matthewlee626@gmail.com>
2020-05-08 10:45:32 -0700

    flower airtable embed

--- src/App.js
+++ src/App.js
@@ -11,11 +11,24 @@ class App extends React.Component {
     };
   }
 
+
   componentDidMount() {
+    var possible = []
+    for(var j = 0; j < 30; j++){
+      possible.push(j)
+    }
+
+    var numbers = []
+    
+    for(var i = 0; i < 3; i++){
+      var index = Math.floor(Math.random()*possible.length)
+      numbers.push(possible.splice(index, 1))
+    }
+
     this.setState({
-      first: ("00" + parseInt(Math.random()*30)).slice(-3),
-      second: ("00" + parseInt(Math.random()*30)).slice(-3),
-      third: ("00" + parseInt(Math.random()*30)).slice(-3)
+      first: ("00" + numbers[0]).slice(-3),
+      second: ("00" + numbers[1]).slice(-3),
+      third: ("00" + numbers[2]).slice(-3)
     })
   }
 
@@ -32,21 +45,22 @@ class App extends React.Component {
           <img src={"./imgs/" + this.state.third +".jpg"} className="identify" alt="strawberry 3"/>
         </div>
         <div className="options">
-        <form action="https://airtable-api-626.herokuapp.com/api" method="POST">
-            <input type="text" id="name" name="name" placeholder="Name" required/>
-            <input type="hidden" id="pic1" name="pic1" value={parseInt(this.state.first)}/>
-            <input type="hidden" id="pic2" name="pic2" value={parseInt(this.state.second)}/>
-            <input type="hidden" id="pic3" name="pic3" value={parseInt(this.state.third)}/>
-            <select name="match" id="match">
-              <option value="0">None</option>
-              <option value="1">1,2</option>
-              <option value="2">1,3</option>
-              <option value="3">2,3</option>
-              <option value="4">1,2,3</option>
-            </select>
-            <button class="submit" type="submit">Submit</button>
-          </form>
+          <form action="https://airtable-api-626.herokuapp.com/api" method="POST">
+              <input type="text" id="name" name="name" placeholder="Name" required/>
+              <input type="hidden" id="pic1" name="pic1" value={parseInt(this.state.first)}/>
+              <input type="hidden" id="pic2" name="pic2" value={parseInt(this.state.second)}/>
+              <input type="hidden" id="pic3" name="pic3" value={parseInt(this.state.third)}/>
+              <select name="match" id="match">
+                <option disabled value="0">None</option>
+                <option value="1">1,2</option>
+                <option value="2">1,3</option>
+                <option value="3">2,3</option>
+                <option value="4">1,2,3</option>
+              </select>
+              <button class="submit" type="submit">Submit</button>
+            </form>
         </div>
+        <iframe class="airtable-embed" title="data" src="https://airtable.com/embed/shrTzj6Gg4WlzSGrc?backgroundColor=red&viewControls=on" frameborder="0" onmousewheel="" width="75%" height="500px" style={{margin: "50px", background: "transparent", border: "1px solid #ccc"}}></iframe>
       </div>
     );
   }
